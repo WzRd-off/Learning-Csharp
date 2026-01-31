33339bba7a1911f04f02a53080310291d350aa4c
Denislav Volin-Danylov <netranner22845@gmail.com>
2026-01-31 12:49:14 +0200

    add pr2_2

---
+++ College/PR2_2/PR2_2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR2_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onSelected(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem lbi = e.Source as ListBoxItem;
+            string shape = lbi.Content.ToString();
+            if (shape == "Квадрат") { }
+            else if (shape == "Паралелограм") { }
+            else if (shape == "Трикутник") { }
+            else if (shape == "Коло") { }
+            else if (shape == "Ромб") { }
+            else { }
+        }
+    }
+}
